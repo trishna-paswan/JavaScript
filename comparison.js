eb30c50e7c47c39e0175d2abc4acfd760ceeedf6
trishna-paswan <S24CSEU0083@bennett.edu.in>
2025-07-16 02:08:29 +0530

    new commands

--- comparison.js
+++ comparison.js
@@ -1,3 +1,19 @@
 console.log(2 > 1); // true
 console.log(2 < 1); // false
 console.log(2 >= 1); // true
+console.log(2 <= 1); // false
+console.log(2 == 1); // false (loose equality)
+console.log(2 !== 1); // true (strict inequality)
+console.log(2 == 1); // false (loose equality)
+console.log(2 != 1); // true (loose inequality)
+
+console.log("2" > 2); // false (string "2" is converted to number 2)
+console.log("2" < 2); // false (string "2" is converted to number 2)
+
+console.log(null == 0); // false (null is not equal to 0)
+console.log(null >= 0); // true (null is considered less than or equal to 0)
+console.log(null < 0); // false (null is not less than 0)
+
+console.log(undefined == 0); // false (undefined is not equal to 0)
+console.log(undefined >= 0); // false (undefined is not greater than or equal to 0)
+console.log(undefined < 0); // false (undefined is not less than 0)
