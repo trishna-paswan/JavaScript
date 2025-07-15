1018a3f2f70297b51af49d4536b2c75f997164a5
trishna-paswan <S24CSEU0083@bennett.edu.in>
2025-07-16 01:04:03 +0530

    data

--- dataTypes.js
+++ dataTypes.js
@@ -1,2 +1,15 @@
 "use strict"; // treat all JS code as newer version
  
+// alert(3+3) // we can use alert to show output in browser
+
+console.log(3+3)
+
+let name = "Trishna Paswan";
+let age = 19;
+let isLoggedIn = true;
+
+// Number -> 2 to the power 53 
+// bigInt -> 2 to the power 53 and above
+// string -> "Trishna Paswan"
+// boolean -> true or false
+// 
