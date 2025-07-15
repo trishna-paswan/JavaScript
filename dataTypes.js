7d2ae384f6e7f4c7c7a2e005856dfcd8be8ae3f0
trishna-paswan <S24CSEU0083@bennett.edu.in>
2025-07-16 01:06:20 +0530

    symbol

--- dataTypes.js
+++ dataTypes.js
@@ -7,9 +7,14 @@ console.log(3+3)
 let name = "Trishna Paswan";
 let age = 19;
 let isLoggedIn = true;
+let state = null; // state is not assigned any value yet
+let height; // height is declared but not assigned any value
+
 
 // Number -> 2 to the power 53 
 // bigInt -> 2 to the power 53 and above
 // string -> "Trishna Paswan"
 // boolean -> true or false
-// 
+// null -> empty value
+// undefined -> variable is declared but not assigned any value
+// symbol -> unique and immutable value, often used as object property keys
