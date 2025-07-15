eec43516e016cf0fce62da451a6d24a769eecad2
trishna-paswan <S24CSEU0083@bennett.edu.in>
2025-07-16 01:42:03 +0530

    null

--- conversionOperations.js
+++ conversionOperations.js
@@ -1,4 +1,13 @@
-let score = "33"; // score is a string
+let score = "33abc"; 
+let value = null
 
 console.log(typeof score); 
 console.log(typeof(score));
+
+let valueInNumber = Number(score); // Convert string to number
+let valueInNull = Number(value); // Convert null to number
+
+console.log(typeof valueInNull);
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); // NaN (Not a Number)
+console.log(valueInNull); // 0 (null is converted to 0)
