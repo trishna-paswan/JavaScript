5abce4177acc31d7a2cdae5a040b2106d47c34bc
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-16 21:33:58 +0530

    string

---
+++ strings.js
@@ -0,0 +1,9 @@
+const name = "hitesh"
+const repoCount = 10
+
+console.log(`Hello, my name is ${name} and I have ${repoCount} repositories.`); 
+// more reliable way to do this (string interpolation)
+
+console.log("Hello, my name is " + name + " and I have " + repoCount + " repositories.");
+// less reliable way to do this (string concatenation)
+
