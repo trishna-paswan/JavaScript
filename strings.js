2c91f0004be246932beb3a890236c8b7088c5325
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-16 22:35:46 +0530

    new commands of strings

--- strings.js
+++ strings.js
@@ -7,3 +7,26 @@ console.log(`Hello, my name is ${name} and I have ${repoCount} repositories.`);
 console.log("Hello, my name is " + name + " and I have " + repoCount + " repositories.");
 // less reliable way to do this (string concatenation)
 
+const gameName = new String("Counter Strike");
+console.log(gameName); // String { "Counter Strike" }
+
+console.log(gameName[0]); // C
+console.log(gameName.__proto__); // String.prototype
+
+console.log(gameName.length); // 14
+console.log(gameName.toUpperCase()); // COUNTER STRIKE
+console.log(gameName.charAt(2)); // u
+console.log(gameName.indexOf("S")); // 8
+
+const newString = gameName.substring(0, 7); // start number, end number
+console.log(newString); // Counter
+
+console.log(gameName.slice(0, 7)); // Counter
+console.log(gameName.slice(-7)); // Strike 
+
+const newStringtwo = "      trishna     ";
+console.log(newStringtwo.trim()); // trishna
+console.log(newStringtwo);
+
+const url = "https://www.google%20com";
+console.log(url.replace("%20", ".")); // https://www.google.com
