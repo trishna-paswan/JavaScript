47c31a0a2d298b04e83b722598d36f7244e5460e
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-18 00:01:39 +0530

    to string method

---
+++ date_time.js
@@ -0,0 +1,9 @@
+console.log("Date and Time Operations"); 
+
+// Get the current date and time
+const currentDate = new Date();
+console.log("Current Date and Time:", currentDate);
+console.log("Current Date:", currentDate.toDateString());
+
+// Get the current date
+const currentYear = currentDate.getFullYear();
