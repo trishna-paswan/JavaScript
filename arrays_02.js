765ca1f70e71cbf9c8d6b85f0a32e2b9c1d84ffc
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-18 01:41:26 +0530

    of commands

--- arrays_02.js
+++ arrays_02.js
@@ -13,3 +13,18 @@ console.log(marvelHeroes); // Output: [ 'thor', 'ironman', 'hulk', 'spiderman', 
 const Another = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [11, 12]]];
 const reallyAnother = Another.flat(Infinity); // return all elements in a single array
 console.log(reallyAnother); // Output: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+
+console.log(Array.isArray(Another)); // Output: true
+console.log(Array.isArray("Hello")); // Output: false
+
+console.log(Array.from("Hello")); // Output: [ 'H', 'e', 'l', 'l', 'o' ]
+
+// Interesting behavior with Array.from
+console.log(Array.from({ name: "trishna"})); // Output: []
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+let scores = Array.of(score1, score2, score3);
+console.log(scores); // Output: [ 100, 200, 300 ]
