4497b1348088e547aae02777e128b7fa32d51b29
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-18 02:02:53 +0530

    freeze

--- Objects.js
+++ Objects.js
@@ -1,15 +1,24 @@
 // Singleton object 
 // Objects literal
 
+const Sym = Symbol("unique"); // Using Symbol to create a unique property
 
-const Objects = {
+const myObjects = {
     name: "Trishna", "Full Name": "Trishna Paswan",
+    [Sym]: "Paswan",
     age: 25,
     isStudent: true,
     lastlogin: ["Monday", "Tuesday", "Wednesday"]
 }
 
 // Accessing object properties
-console.log(Objects.name); // Output: Trishna
-console.log(Objects["age"]); // Output: 25
-console.log(Objects["Full Name"]); // Output: Trishna Paswan
+console.log(myObjects.name); // Output: Trishna
+console.log(myObjects["age"]); // Output: 25
+console.log(myObjects["Full Name"]); // Output: Trishna Paswan
+console.log(myObjects[Sym]); // Output: Paswan
+
+Object.age = 26; // Updating age
+console.log(myObjects.age); // Output: 26
+
+Object.freeze(myObjects); // Freezing the object to prevent further modifications
+console.log(myObjects);
