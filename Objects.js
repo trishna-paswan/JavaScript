70ecaa62b101733e3bbdc8cc1d016b51812fa00c
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-18 01:52:03 +0530

    accessing objects

---
+++ Objects.js
@@ -0,0 +1,15 @@
+// Singleton object 
+// Objects literal
+
+
+const Objects = {
+    name: "Trishna", "Full Name": "Trishna Paswan",
+    age: 25,
+    isStudent: true,
+    lastlogin: ["Monday", "Tuesday", "Wednesday"]
+}
+
+// Accessing object properties
+console.log(Objects.name); // Output: Trishna
+console.log(Objects["age"]); // Output: 25
+console.log(Objects["Full Name"]); // Output: Trishna Paswan
