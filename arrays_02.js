fb9a4156b4217943b0b26459fef7d169d7a55780
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-18 01:36:59 +0530

    flat command

--- arrays_02.js
+++ arrays_02.js
@@ -9,3 +9,7 @@ console.log(allHeroes); // Output: [ 'thor', 'ironman', 'hulk', 'spiderman', 'ca
 
 marvelHeroes.push(dc);
 console.log(marvelHeroes); // Output: [ 'thor', 'ironman', 'hulk', 'spiderman', 'captain america', [ 'superman', 'batman', 'flash', 'green lantern' ] ]
+
+const Another = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [11, 12]]];
+const reallyAnother = Another.flat(Infinity); // return all elements in a single array
+console.log(reallyAnother); // Output: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
