66ff4d0cc6754a04df0f0a76834d886b68c2560a
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-19 02:24:47 +0530

    ARRAY FUNCTIONS

--- functions.js
+++ functions.js
@@ -47,3 +47,30 @@ function calculateTotalPrice(val1, val2, ...prices) {
     return prices
 }
 console.log(calculateTotalPrice(100, 200, 300, 400)); // Output: [ 300, 400 ]
+
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
