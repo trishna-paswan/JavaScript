ab392258e93ce9c91177e487e4768da6ae4e9f4e
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-21 23:31:21 +0530

    detailed code

--- reduce.js
+++ reduce.js
@@ -1,6 +1,7 @@
 const Nums = [1, 2, 3, 4, 5];
 // Using reduce to sum numbers
 const sum = Nums.reduce((accumulator, currentValue) => {
+    console.log("Accumulator:", accumulator, "Current Value:", currentValue);
     return accumulator + currentValue;
 }, 0); // Initial value is 0
 console.log("Sum of Numbers:", sum); // Output: Sum of Numbers: 15
