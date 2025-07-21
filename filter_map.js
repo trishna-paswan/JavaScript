d52e605021f541e67b9b3e80b0306ab5cdf60478
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-21 13:03:59 +0530

    forEach_Filter

--- filter_map.js
+++ filter_map.js
@@ -1,4 +1,14 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 // Using filter to get even numbers
 const evenNums = myNums.filter(num => num % 2 === 0);
 console.log("Even Numbers:", evenNums);
+
+// Using filter to get odd numbers using forEach
+const oddNums = [];
+myNums.forEach(num => {
+    if (num % 2 !== 0) {
+        oddNums.push(num);
+    }
+});
+console.log("Odd Numbers:", oddNums);
